4dcdb347db068836fe8abe1bc1fe214a84d316ad
Elise Muellerleile <eliamue@live.com>
2021-11-29 22:20:08 -0600

    demo convos styled and fleshed out

--- src/components/demo/DemoConvo1.jsx
+++ src/components/demo/DemoConvo1.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import DemoHeader from './DemoHeader';
+import styles from '../../styles/conversations.css';
 
 export default class DemoConvo1 extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.convo_container}>
         <DemoHeader />
-        Hi it be Sbeve
+        <h4 className={styles.from_container}>
+          <p className={styles.from_item}>From: Gary Busey</p>
+          <p className={styles.from_item}>Date: Friday, October 15, 2021</p>
+        </h4>
+
+        <article className={styles.message_container}>
+          <p className={styles.message_p}>Add me on insta, we should talk kpop!</p>
+        </article>
       </div>
     );
   }
