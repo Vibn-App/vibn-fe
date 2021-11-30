9ed3686327dbf0bd382308fe6b9440974047af06
Elise Muellerleile <eliamue@live.com>
2021-11-30 00:38:03 -0600

    create conversation page for demo styled

--- src/components/demo/DemoConversations.jsx
+++ src/components/demo/DemoConversations.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import DemoHeader from './DemoHeader';
 import styles from '../../styles/conversations.css';
+import linebreak from '../../assets/linebreak.png';
 
 export default function Conversations() {
   return (
     <div className={styles.top_container}>
       <DemoHeader />
       <h1 className={styles.message_title}>Your Messages</h1>
+      <img src={linebreak} alt="linebreak" />
       <main className={styles.main_container}>
         <Link to="/demo-convo/detail/demo1">
           <h1>Gary Busey</h1>
--- src/components/demo/DemoCreateConvo.jsx
+++ src/components/demo/DemoCreateConvo.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import styles from '../../styles/createmsg.css';
+import linebreak from '../../assets/linebreak.png';
+import DemoHeader from './DemoHeader';
 
 export default class DemoCreateConvo extends Component {
   render() {
-    return <div>create a convo bitch</div>;
+    return (
+      <div className={styles.create_container}>
+        <DemoHeader />
+        <img src={linebreak} alt="linebreak" />
+        <div className={styles.send_msg_header}>This is where you would start a conversation!</div>
+      </div>
+    );
   }
 }
